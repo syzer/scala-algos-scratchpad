f29ecf93891ff43eabfbfd77b59723195e78bd40
syzer3@gmail.com <syzer3@gmail.com>
2019-08-07 17:17:56 +0200

    * Case classes * WIP: ordering

--- src/Classes.scala
+++ src/Classes.scala
@@ -21,6 +21,44 @@ case class Doctor(firstName: String,
                   group: String) extends Employee(firstName, lastName,
   department, salary)
 
-val eng1 = Engineer("Isaac", "Newton", "IT", 4500.50, "Engineering")
+val eng1 = Engineer("Isaac", "Newton", "IT", 4530.50, "Engineering")
+val eng2 = Engineer("Albert", "Einstein", "Infra", 4600.50, "Engineering")
 
-println(eng1)
+println(eng1, eng2)
+
+val doc1 = Doctor("Michael", "Young", "Cardio", 2000.5, "Medicine")
+val doc2 = Doctor("Jeffrey", "Hall", "Pathology", 1100.5, "Medicine")
+
+val engineers = List(eng1, eng2)
+val doctors = List(doc1, doc2)
+val employees = engineers ::: doctors
+
+val emp2 = List(engineers, doctors).flatten
+println(emp2)
+println(employees.contains(doc1)) // true
+println("-=======-")
+
+
+// Now, let’s take another problem and develop an application using lists.
+// Our task is to create a mini-dictionary containing at least the following words:
+// apple, cow, color, god, goat, dog, house, mother, orange, rat, zeal, university.
+// Here are the additional requirements:
+// 1. Sort the words in ascending order.
+// 2. Find whether there are duplicate words in the dictionary.
+// 3. Find the total number of words in the dictionary.
+// 4. Find whether the word “monkey” exists. Also check whether the word “university” exists.
+// 5. Use immutable APIs as much as possible; however, you may use println for manual validation.
+
+abstract class Thing(name: String) {
+  implicit def ordering[A <: Thing]: Ordering[A] = Ordering.by(_.toString)
+}
+
+//class Word(name: String) extends Thing(name)
+
+case class Animal(name: String) extends Ordered Thing(name)
+
+case class Fruit(name: String, taste: String) extends Thing(name)
+
+//val dict = List(Animal("rat"), Fruit("apple"), Thing("color"), Thing("university"))
+val dict = List(Animal("rat"))
+println(dict.sorted)
