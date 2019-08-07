773140e226e9a86b5e251e681771eb60ec35f658
syzer3@gmail.com <syzer3@gmail.com>
2019-08-07 18:09:28 +0200

    Excercises

--- src/excercises/4.3.scala
+++ src/excercises/4.3.scala
@@ -7,3 +7,26 @@
 
 val elems = List(1, 2, 3, 4, 5)
 println(elems)
+elems.foldLeft(0)(_ + _)
+
+def indexOf(elems: List[Int], elem: Int): Int = {
+  val index = -1
+  elems.foldLeft(0)((acc, curr) => {
+    //    println(x, i)
+    if (curr == elem) {
+      println(index)
+    }
+    -1
+  })
+  //  -1
+}
+
+val num1 = indexOf(elems, 2)
+println(num1)
+
+println("Answer ", 3, "Reverse",
+  elems.reverse)
+println("Answer ", 4, "Last elem",
+  elems.foldLeft(0)((acc, e) => e))
+println("Answer ", 5, "Length",
+  elems.foldLeft(0)((acc, e) => acc + 1))
